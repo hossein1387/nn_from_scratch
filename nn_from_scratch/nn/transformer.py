928d8a4bbabab3770e6b698d0312d310599908a5
hossein1387 <m.h.askari.hemmat@gmail.com>
2024-06-13 22:56:47 -0400

    Workign transformer

--- nn_from_scratch/nn/transformer.py
+++ nn_from_scratch/nn/transformer.py
@@ -1,31 +1,27 @@
+from typing import Any
 import numpy as np
 from .linear import Linear
 from .funcs import softmax
 
 class MultiHeadAttention:
-
     def __init__(self, nheads, d_model):
         assert (d_model%nheads==0), "Embedding dimension must be divisible by number of attention heads"
         self.nheads = nheads
         self.d_k = int(d_model/nheads)
         self.d_model = d_model
-
-        self.q = Linear([d_model, d_model])
-        self.k = Linear([d_model, d_model])
-        self.v = Linear([d_model, d_model])
-        self.o = Linear([d_model, d_model])
-
+        self.q_w = Linear([d_model, d_model])
+        self.k_w = Linear([d_model, d_model])
+        self.v_w = Linear([d_model, d_model])
+        self.o_w = Linear([d_model, d_model])
     def scaled_dot_product_attention_mine(self, q, k, v):
             scale_factor = 1/np.sqrt(q.shape[-1])
             attn_scores = q@k.transpose(-2, -1) * scale_factor
             attn_probs = softmax(attn_scores, axis=-1)
             output = attn_probs@v
             return output
-
     def split_heads(self, x):
         batch_size, seq_length, d_model = x.shape()
         return x.view(batch_size, seq_length, self.nheads, self.d_k).transpose(1, 2)
-
     def forward(self, Q, K, V):
         batch_size, seq_length, _ = Q.shape()
         Q = self.q(self.split_heads(Q))
@@ -35,6 +31,8 @@ class MultiHeadAttention:
         output = output.transpose(1, 2).contiguous().view(batch_size, seq_length, self.d_model)
         output = self.o(output)
         return output
+    def __call__(self, q, k, v):
+         return self.forward(q, k, v)
 
 class Transformer:
     def __init__(self, nheads, d_model):
